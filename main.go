f050dfc10f10f41be58ecc97355007790d0ea20a
Fazel <fazelsamar@gmail.com>
2023-06-06 01:10:17 +0330

    Add image upload and reserve mediafiles

--- main.go
+++ main.go
@@ -6,14 +6,15 @@ import (
 	"github.com/fazelsamar/go-ecommerce/middleware"
 	"github.com/fazelsamar/go-ecommerce/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func setupRoutes(app *fiber.App) {
 	// Product routes
 	app.Get("/products", models.GetProducts)
-	app.Get("/product/:id", models.GetProduct)
-	app.Post("/product", middleware.RequireAuth, middleware.IsAdmin, models.NewProducts)
-	app.Delete("/product/:id", middleware.RequireAuth, middleware.IsAdmin, models.DeleteProducts)
+	app.Get("/products/:id", models.GetProduct)
+	app.Post("/products", middleware.RequireAuth, middleware.IsAdmin, models.NewProducts)
+	app.Delete("/products/:id", middleware.RequireAuth, middleware.IsAdmin, models.DeleteProducts)
 
 	//Auth routes
 	app.Post("/register", models.Register)
@@ -28,7 +29,13 @@ func main() {
 
 	app := fiber.New()
 
+	// Use the CORS middleware with default options
+	app.Use(cors.New())
+
+	// Serve static files from the "mediafiles" directory
+	app.Static("/mediafiles", "./mediafiles")
+
 	setupRoutes(app)
 
-	app.Listen(":3000")
+	app.Listen(":8000")
 }
---
+++ models/fileUtils.go
@@ -0,0 +1,94 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+var uploadPath string = "mediafiles"
+
+// SaveUploadedFile saves the uploaded image file and returns the file path or an error
+func saveUploadedFile(file *multipart.FileHeader) (string, error) {
+	src, err := file.Open()
+	if err != nil {
+		return "", err
+	}
+	defer src.Close()
+
+	// Validate file MIME type
+	if !strings.HasPrefix(file.Header.Get("Content-Type"), "image/") {
+		return "", errors.New("Invalid file type. Only images are allowed")
+	}
+
+	// Create a unique file name
+	filename := fmt.Sprintf("%d_%s", time.Now().Unix(), filepath.Base(file.Filename))
+
+	// Specify the path to save the file
+	savePath := filepath.Join(uploadPath, filename)
+
+	// Create the directory if it doesn't exist
+	if err := createDirectory(uploadPath); err != nil {
+		return "", err
+	}
+
+	// Create the destination file
+	dst, err := os.Create(savePath)
+	if err != nil {
+		return "", err
+	}
+	defer dst.Close()
+
+	// Copy the uploaded file to the destination file
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return "", err
+	}
+
+	// Convert the file path separators to forward slashes
+	savePath = filepath.ToSlash(savePath)
+
+	return savePath, nil
+}
+
+// Helper function to create the directory if it doesn't exist
+func createDirectory(dirPath string) error {
+	// Check if the directory exists
+	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
+		// Create the directory with 0755 permissions
+		err := os.MkdirAll(dirPath, 0755)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Helper function to generate a unique file path for the uploaded file
+func generateFilePath(filename string) string {
+	// Get the current working directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		return ""
+	}
+
+	// Create a new directory uploadPath if it doesn't exist
+	uploadsDir := filepath.Join(cwd, uploadPath)
+	if _, err := os.Stat(uploadsDir); os.IsNotExist(err) {
+		os.Mkdir(uploadsDir, 0755)
+	}
+
+	// Generate a unique file name using a timestamp and the original file name
+	fileName := fmt.Sprintf("%d_%s", time.Now().Unix(), filename)
+
+	// Construct the full file path by combining the uploads directory and the unique file name
+	filePath := filepath.Join(uploadsDir, fileName)
+
+	return "/" + filepath.ToSlash(filePath)
+}
